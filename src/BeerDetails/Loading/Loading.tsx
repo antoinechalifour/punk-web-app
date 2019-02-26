eff487d576b967c4def941bcfc146a74458a4ddb
antoinechalifour <antoine.chalifour@gmail.com>
2019-02-26 22:11:28 +0100

    feat: lighter gray on skeleton loader

--- src/BeerDetails/Loading/Loading.tsx
+++ src/BeerDetails/Loading/Loading.tsx
@@ -8,14 +8,14 @@ export interface LoadingProps {}
 export const Loading: React.FunctionComponent<LoadingProps> = () => (
   <Template
     brewersTips={<TextBlock rows={3} color="rgba(255, 255, 255, .2)" />}
-    description={<TextBlock rows={4} color="rgba(0, 0, 0, .2)" />}
-    generalInformation={<TextBlock rows={2} color="rgba(0, 0, 0, .2)" />}
+    description={<TextBlock rows={4} color="rgba(0, 0, 0, .1)" />}
+    generalInformation={<TextBlock rows={2} color="rgba(0, 0, 0, .1)" />}
     header={
       <div>
         <TextBlock rows={2} color="rgba(255, 255, 255, .2)" />
       </div>
     }
-    hops={<TextBlock rows={5} color="rgba(0, 0, 0, .2)" />}
+    hops={<TextBlock rows={5} color="rgba(0, 0, 0, .1)" />}
     image={
       <RoundShape
         color="#f5f5f5"
@@ -28,7 +28,8 @@ export const Loading: React.FunctionComponent<LoadingProps> = () => (
         }}
       />
     }
-    malts={<TextBlock rows={3} color="rgba(0, 0, 0, .2)" />}
-    yeast={<TextBlock rows={1} color="rgba(0, 0, 0, .2)" />}
+    malts={<TextBlock rows={3} color="rgba(0, 0, 0, .1)" />}
+    yeast={<TextBlock rows={1} color="rgba(0, 0, 0, .1)" />}
+    foodPairing={<TextBlock rows={2} color="rgba(0, 0, 0, .1)" />}
   />
 );
