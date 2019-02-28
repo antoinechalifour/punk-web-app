458bc6a4a8d6f06c71213fb6b6b8e0238b68e93b
antoinechalifour <antoine.chalifour@gmail.com>
2019-02-28 21:08:10 +0100

    fix: cache index.html in sw

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  staticFileGlobs: ["build/static/css/**.css", "build/static/js/**.js"],
+  staticFileGlobs: [
+    "build/index.html",
+    "build/static/css/**.css",
+    "build/static/js/**.js"
+  ],
   swFilePath: "./build/service-worker.js",
   stripPrefix: "build/",
   importScripts: ["./service-worker-api.js"],
